28aa62be774a96766ebfdfd8b722127ceeff8a69
stoph01 <99798061+stoph01@users.noreply.github.com>
2022-02-16 12:06:28 +0100

    Create chess.py

---
+++ chess.py
@@ -0,0 +1,19 @@
+# blocks = int(input("enter a number "))
+# levels = 0
+# while levels <= blocks:
+#     levels += 1
+#     blocks = blocks - levels
+# print("the hight of the pyramid is ",levels)    
+
+
+
+
+def happy_new_year(wishes = True):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+    
+        print("Happy New Year!")
+happy_new_year() 
+
